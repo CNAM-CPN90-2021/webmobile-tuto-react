70887240639bfcb0ce264838910555c14a3023a3
geraud.henrion <geraud.henrion@nartex.fr>
2020-12-15 16:13:53 +0100

    Add exercice 6

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { Exercice1 } from "./Exercice1";
 import { Exercice2 } from "./Exercice2";
 import { Exercice3 } from "./Exercice3";
 import { Exercice5 } from "./Exercice5";
+import { Exercice6 } from "./Exercice6";
 import "./App.css";
 
 export function App() {
   return (
     <div className="App">
+      <Exercice6 />
+      <hr />
       <Exercice5 />
       <hr />
       <Exercice3 />
--- src/Exercice6.jsx
+++ src/Exercice6.jsx
@@ -2,15 +2,30 @@ import { useEffect, useState } from "react";
 
 export function Exercice6() {
   const [pokemons, setPokemons] = useState([]);
+  const [limit, setLimit] = useState(50);
 
   useEffect(() => {
-    fetch("")
-      .then(() => {})
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
+      .then((rawResponse) => rawResponse.json())
+      .then((jsonResponse) => {
+        const results = jsonResponse.results;
+        setPokemons(results);
+      })
       .catch(() => {});
-  }, []);
+  }, [limit]);
+
+  console.log(limit);
 
   return (
     <ul className="App">
+      <input
+        type="number"
+        value={limit}
+        onInput={(event) => {
+          setLimit(event.target.value);
+        }}
+      />
+
       {pokemons.map((pokemon) => {
         return <li key={pokemon.name}>{pokemon.name}</li>;
       })}
